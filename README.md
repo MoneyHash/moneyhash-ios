294b114917191eb758ed745123a1105a9218cbb6
Ahmed Salem Elzeiny <ahmedsalemelzeiny2013@gmail.com>
2022-02-10 11:52:59 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MoneyHashiOS
+
+A description of this package.
---
+++ Tests/MoneyHashiOSTests/MoneyHashiOSTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MoneyHashiOS
+
+final class MoneyHashiOSTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MoneyHashiOS().text, "Hello, World!")
+    }
+}
