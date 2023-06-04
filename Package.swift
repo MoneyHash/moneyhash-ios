7ef92d93c153c9d5f7561756f631ea1fbda0209d
ahmedelzeiny <ahmedsalemelzeiny2013@gmail.com>
2023-06-04 15:27:22 +0300

    Version 1.0.1

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .package(
             name: "MoneyHashShared",
             url: "https://github.com/MoneyHash/moneyhash-spm",
-            from: "0.0.1"
+            from: "1.0.1"
         )
     ],
     targets: [
